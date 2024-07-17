4e200948f5afe86d0e876aca15c9a1fd36923641
kseniia <ksusha.karpova2011@yandex.ru>
2024-07-17 19:54:05 +0300

    add tts

---
+++ TTS/src/service/tts_service.py
@@ -0,0 +1,20 @@
+from model.v1.manager import XttsModelManager
+from model import BaseModelManager
+from functools import lru_cache
+from fastapi import Depends
+
+
+class TTSServices:
+
+    def __init__(self, model_manger: BaseModelManager):
+        self.model_manger = model_manger
+
+    def text_to_voice(self, text: str):
+        return self.model_manger.text_to_voice(text=text)
+
+
+@lru_cache()
+def get_tts_service(
+    model_manger: BaseModelManager = Depends(XttsModelManager),
+) -> TTSServices:
+    return TTSServices(model_manger)
